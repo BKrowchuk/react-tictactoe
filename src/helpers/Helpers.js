3b96b4689f09a12157254832fd10533a18f0eb68
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-10-13 21:39:50 -0400

    win helper function

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -5,18 +5,36 @@ export function showNotification(setter) {
   }, 2000);
 }
 
-export function checkWin(correct, wrong, word) {
-  let status = "win";
-
-  // Check for win
-  word.split("").forEach((letter) => {
-    if (!correct.includes(letter)) {
-      status = "";
-    }
-  });
-
-  // Check for lose
-  if (wrong.length === 6) status = "lose";
-
+export function checkWin(grid) {
+  let status = "";
+  let winner = -1;
+  //   Check for win
+  //   rows
+  if (grid[0][0] === grid[0][1] && grid[0][0] === grid[0][2]) {
+    winner = grid[0][0];
+  } else if (grid[1][0] === grid[1][1] && grid[1][0] === grid[1][2]) {
+    winner = grid[1][0];
+  } else if (grid[2][0] === grid[2][1] && grid[1][0] === grid[2][2]) {
+    winner = grid[2][0];
+    // cols
+  } else if (grid[0][0] === grid[1][0] && grid[0][0] === grid[2][0]) {
+    winner = grid[0][0];
+  } else if (grid[0][1] === grid[1][1] && grid[0][1] === grid[2][1]) {
+    winner = grid[0][1];
+  } else if (grid[0][2] === grid[1][2] && grid[0][2] === grid[2][2]) {
+    winner = grid[0][2];
+    // cross
+  } else if (grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) {
+    winner = grid[1][1];
+  } else if (grid[2][0] === grid[1][1] && grid[2][0] === grid[0][2]) {
+    winner = grid[1][1];
+  }
+  if (winner === 0) {
+    status = "O";
+  } else if (winner === 1) {
+    status = "X";
+  }
+  // Check for cats game
+  if (!grid.includes(2)) status = "C";
   return status;
 }
